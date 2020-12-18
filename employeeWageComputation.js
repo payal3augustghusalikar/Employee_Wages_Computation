94725f70bf8390ae5e1e1c7e66cb7254a9a9d8cf
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-18 22:58:45 +0530

    Added : calculate daily employee wages for employee

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,11 +1,22 @@
-console.log(`\n ******* Welcome to employee wage computation ******* \n`);
-console.log(`----------------------------------------------------`);
-
 const isPresent = 1;
+let wagePerHour = 20;
+let fullDayHour = 8;
+let dailyEmpWage = 0;
 
-if(Math.floor(Math.random() * 2) == 1)
+getWelcomeMessage();
+
+function getWelcomeMessage() {
+    console.log(`\n ******* Welcome to employee wage computation ******* \n`);
+    console.log(`----------------------------------------------------`);
+}
+
+if(Math.floor(Math.random() * 2) == 1) {
     console.log(`Employee is Present`);
-else
+    employeeHour = 8; 
+}
+else {
     console.log(`Employee is absent`);
-
-    
+    employeeHour = 0;
+}
+dailyEmpWage = wagePerHour * employeeHour;
+console.log(`Daily employee wage is : ${dailyEmpWage} \n`); 

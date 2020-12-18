79573b83d210f6a612577262a4a1804a10f064d3
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-18 22:31:55 +0530

    Added : check employee present or absent

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -1,2 +1,11 @@
 console.log(`\n ******* Welcome to employee wage computation ******* \n`);
 console.log(`----------------------------------------------------`);
+
+const isPresent = 1;
+
+if(Math.floor(Math.random() * 2) == 1)
+    console.log(`Employee is Present`);
+else
+    console.log(`Employee is absent`);
+
+    

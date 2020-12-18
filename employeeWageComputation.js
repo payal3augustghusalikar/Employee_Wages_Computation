e5ecbc7a059e1875d50d54f1c249b28d612a960c
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-18 23:47:17 +0530

    Refactored : calculate employee wages by switch case

--- employeeWageComputation.js
+++ employeeWageComputation.js
@@ -2,7 +2,7 @@ const isPresentFullTime = 1;
 const isPresentPartTime = 2;
 let wagePerHour = 20;
 let fullDayHour = 8;
-let dailyEmpWage = 0;
+//let dailyEmpWage = 0;
 
 getWelcomeMessage();
 
@@ -10,19 +10,24 @@ function getWelcomeMessage() {
     console.log(`\n ******* Welcome to employee wage computation ******* \n`);
     console.log(`----------------------------------------------------`);
 }
+
 var randomCheck = Math.floor(Math.random() * 3);
 
-if(randomCheck == 1) {
-    console.log(`Employee is Present full time `);
-    employeeHour = 8; 
-}
-else if(randomCheck == 2) {
-    console.log(`Employee is Present part time `);
-    employeeHour = 4;
-}
-else {
-    console.log(`Employee is absent`);
-    employeeHour = 0;
+switch(randomCheck) {
+    case 1:
+        console.log(`Employee is Present full time `);
+        employeeHour = 8; 
+        break;
+
+    case 2:
+        console.log(`Employee is Present part time `);
+        employeeHour = 4;
+        break;
+
+    default:
+        console.log(`Employee is absent`);
+        employeeHour = 0;
+        break;
 }
 dailyEmpWage = wagePerHour * employeeHour;
 console.log(`Daily employee wage is : ${dailyEmpWage} \n`); 
